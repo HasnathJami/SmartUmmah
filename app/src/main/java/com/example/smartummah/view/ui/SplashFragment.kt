3c81c4e65347ecfe44e56614ebc5d2add1875a79
hasnath.jami.chowdhury <hasnath.jami.chowdhury@gmail.com>
2023-02-04 15:01:03 +0600

    Splash Screen Implementation And App Logo Change

--- app/src/main/java/com/example/smartummah/view/ui/SplashFragment.kt
+++ app/src/main/java/com/example/smartummah/view/ui/SplashFragment.kt
@@ -1,19 +1,54 @@
 package com.example.smartummah.view.ui
 
+import android.content.Context
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation.findNavController
 import com.example.smartummah.R
+import com.example.smartummah.databinding.FragmentSplashBinding
 
 class SplashFragment : Fragment() {
+
+    private lateinit var mContext: Context
+    private lateinit var mBinding: FragmentSplashBinding
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mContext = context
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        (activity as MainActivity).supportActionBar?.hide()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_splash, container, false)
+        mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_splash, container, false)
+        mBinding.lifecycleOwner = this
+        return mBinding.root
     }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        populateSplashScreen()
+
+    }
+
+    private fun populateSplashScreen() {
+        Handler(Looper.getMainLooper()).postDelayed({
+            findNavController(mBinding.root)
+                .navigate(R.id.dashboardFragment, null)
+            (activity as MainActivity).supportActionBar?.show()
+        }, 1500)
+        findNavController(mBinding.root).popBackStack(R.id.splashScreenFragment, true);
+    }
 }
